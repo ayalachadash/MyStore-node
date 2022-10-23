9682f8902ec69d8bff42659d81d8ad5fb4b4815c
ayalachadash <ayala0533121955@gmail.com>
2022-10-23 23:08:07 +0300

    send email

--- Routers/User.router.js
+++ Routers/User.router.js
@@ -1,11 +1,12 @@
-const express = require('express')
+const express = require('express');
+const { sendMailToNewMember } = require('../BL/SendEmail');
 const router = express.Router();
 
 const UserBL = require('../BL/UserBL')
 //new user
 router.route('/signUp')
     .post(async function (req, res) {
-        
+
         let obj = req.body;
         let data = await UserBL.signUp(obj);
         return res.json(data);
@@ -33,6 +34,15 @@ router.route('/')
     .get(async function (req, res) {
         let data = await UserBL.getAll();
         res.status(200).json(data);
+    })
+
+
+router.route('/sendEmail')
+    .post(async function (req, res) {
+        console.log("hello send email");
+        let obj = req.body;
+        let data = await sendMailToNewMember(obj.mail, obj.name, obj.txt);
+        return res.json(data);
     })
 
 module.exports = router;
